2fc2ad61d1c101fd32c95ed01aa17bca895f59f7
jarekkopa <jarek.kopa@gmail.com>
2018-05-12 12:08:39 +0200

    Gotowy prosty formularz do rejestracji użytkownika

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserController extends Controller
 {
-    /**
-     * @Route("/user", name="user")
-     */
-    public function userAdd()
+    public function userAdd(Request $request)
     {
-        return $this->render('user/index.html.twig', [
+        $newUser = new User();
+        $form = $this->createFormBuilder($newUser)
+            ->add('login', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newUser);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_add');
+        }
+
+        return $this->render('user/add.html.twig', [
             'controller_name' => 'UserController',
+            'form' => $form->createView(),
         ]);
     }
 }
