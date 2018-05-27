6d528bffbd0ddb1b54fb5bf2b144a9cca5a669b1
jarekkopa <jarek.kopa@gmail.com>
2018-05-27 11:16:40 +0200

    Poprawki w usuwaniu konta użytkownika

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,16 +8,6 @@ use App\Entity\UserEntity;
 
 class UserController extends Controller
 {
-    // /**
-    //  * @Route("/user", name="user")
-    //  */
-    // public function deleteUser()
-    // {
-    //     return $this->render('user/delete_user.html.twig', [
-    //         'controller_name' => 'UserController',
-    //     ]);
-    // }
-
     public function deleteUser($id)
     {
         $user = $this->getDoctrine()->getRepository(UserEntity::class)->find($id);
