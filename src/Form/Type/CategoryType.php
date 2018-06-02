43fdaa746be91dd49d8f07c698d2f9c2305a3a0f
beny1981 <benicewicz@gmail.com>
2018-06-02 10:33:27 +0200

    Utworzenie pliku i dodanie pierwszego kodu do tworzenia formularza subkategorii

---
+++ src/Form/Type/CategoryType.php
@@ -0,0 +1,43 @@
+<?
+namespace App\Forms\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('adv_category', EntityType::class, array(
+                'class'       => 'App\Entity\CategoryEntity',
+                'placeholder' => '',
+            ))
+        ;
+
+        $builder->addEventListener(
+            FormEvents::PRE_SET_DATA,
+            function (FormEvent $event) {
+                $form = $event->getForm();
+
+                // this would be your entity
+                $data = $event->getData();
+
+                $category = $data->getAdvCategory();
+                $subcategory = null === $category ? array() : $category->getSubcategory();
+
+                $form->add('subcategory', EntityType::class, array(
+                    'class' => 'App\Entity\SubcategoryEntity',
+                    'placeholder' => '',
+                    'choices' => $subcategory,
+                ));
+            }
+        );
+    }
+
+    // ...
+}
