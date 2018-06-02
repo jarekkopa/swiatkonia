142048fcc84d96889109f81bac9a01af9877352a
beny1981 <benicewicz@gmail.com>
2018-06-02 10:19:12 +0200

    Wyczyszczenie encji subkategorii

--- src/Migrations/Version20180602081758.php
+++ src/Migrations/Version20180602081758.php
@@ -8,15 +8,13 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180602075456 extends AbstractMigration
+final class Version20180602081758 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('CREATE TABLE subcategory_entity (id INTEGER NOT NULL, category_id_id INTEGER DEFAULT NULL, subcategory_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_BC1D81889777D11E ON subcategory_entity (category_id_id)');
         $this->addSql('DROP INDEX IDX_54F1F40B9458D7F4');
         $this->addSql('DROP INDEX IDX_54F1F40BA76ED395');
         $this->addSql('DROP INDEX IDX_54F1F40BC7209D4F');
@@ -28,6 +26,13 @@ final class Version20180602075456 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_54F1F40B9458D7F4 ON advert (adv_category_id)');
         $this->addSql('CREATE INDEX IDX_54F1F40BA76ED395 ON advert (user_id)');
         $this->addSql('CREATE INDEX IDX_54F1F40BC7209D4F ON advert (region_id_id)');
+        $this->addSql('DROP INDEX IDX_BC1D81889777D11E');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__subcategory_entity AS SELECT id, category_id_id, subcategory_name FROM subcategory_entity');
+        $this->addSql('DROP TABLE subcategory_entity');
+        $this->addSql('CREATE TABLE subcategory_entity (id INTEGER NOT NULL, category_id_id INTEGER DEFAULT NULL, subcategory_name VARCHAR(255) DEFAULT NULL COLLATE BINARY, PRIMARY KEY(id), CONSTRAINT FK_BC1D81889777D11E FOREIGN KEY (category_id_id) REFERENCES category_entity (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO subcategory_entity (id, category_id_id, subcategory_name) SELECT id, category_id_id, subcategory_name FROM __temp__subcategory_entity');
+        $this->addSql('DROP TABLE __temp__subcategory_entity');
+        $this->addSql('CREATE INDEX IDX_BC1D81889777D11E ON subcategory_entity (category_id_id)');
     }
 
     public function down(Schema $schema) : void
@@ -35,7 +40,6 @@ final class Version20180602075456 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('DROP TABLE subcategory_entity');
         $this->addSql('DROP INDEX IDX_54F1F40B9458D7F4');
         $this->addSql('DROP INDEX IDX_54F1F40BA76ED395');
         $this->addSql('DROP INDEX IDX_54F1F40BC7209D4F');
@@ -47,5 +51,12 @@ final class Version20180602075456 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_54F1F40B9458D7F4 ON advert (adv_category_id)');
         $this->addSql('CREATE INDEX IDX_54F1F40BA76ED395 ON advert (user_id)');
         $this->addSql('CREATE INDEX IDX_54F1F40BC7209D4F ON advert (region_id_id)');
+        $this->addSql('DROP INDEX IDX_BC1D81889777D11E');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__subcategory_entity AS SELECT id, category_id_id, subcategory_name FROM subcategory_entity');
+        $this->addSql('DROP TABLE subcategory_entity');
+        $this->addSql('CREATE TABLE subcategory_entity (id INTEGER NOT NULL, category_id_id INTEGER DEFAULT NULL, subcategory_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO subcategory_entity (id, category_id_id, subcategory_name) SELECT id, category_id_id, subcategory_name FROM __temp__subcategory_entity');
+        $this->addSql('DROP TABLE __temp__subcategory_entity');
+        $this->addSql('CREATE INDEX IDX_BC1D81889777D11E ON subcategory_entity (category_id_id)');
     }
 }
